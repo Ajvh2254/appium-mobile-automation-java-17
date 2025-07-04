21ce957ad9eeeaa9dd9f5b6e603db38961d9f2e6
anthonyhenderson <cuzixsin@gmail.com>
2025-07-03 23:08:57 -0700

    new BaseTests class

--- src/main/java/com/apps/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.apps.config;
-
-public class Config {
-
-    private static final Boolean DEFAULT_AUTO_EMULATOR_START = false;
-    public static final Boolean AUTO_EMULATOR_START = Boolean.parseBoolean(System.getProperty("autoEmulatorStart", DEFAULT_AUTO_EMULATOR_START.toString()));
-
-}
