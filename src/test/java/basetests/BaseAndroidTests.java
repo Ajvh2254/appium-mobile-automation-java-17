9783c40ba934fbafeb5a2cfbe4abad2280c28da3
anthonyhenderson <cuzixsin@gmail.com>
2025-07-12 12:48:29 -0700

    new AppiumDriverManager starts and stops appium

--- src/test/java/basetests/BaseAndroidTests.java
+++ src/test/java/basetests/BaseAndroidTests.java
@@ -1,4 +1,24 @@
 package basetests;
 
+import appiumfactory.driver.AppiumDriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseAndroidTests {
+
+    @BeforeMethod
+    public void initiateAppium() {
+        AppiumDriverManager.startAppium();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        AppiumDriverManager.stopAppium();
+    }
+
 }
--- src/test/java/basetests/BaseIosTests.java
+++ src/test/java/basetests/BaseIosTests.java
@@ -1,4 +1,18 @@
 package basetests;
 
+import appiumfactory.driver.AppiumDriverManager;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseIosTests {
+
+    @BeforeMethod
+    public void initiateIos() {
+        AppiumDriverManager.startAppium();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
