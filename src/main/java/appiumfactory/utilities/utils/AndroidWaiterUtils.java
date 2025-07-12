f12620a0398881eadcb74a22d501876c50cadcb1
anthonyhenderson <cuzixsin@gmail.com>
2025-07-12 14:49:02 -0700

    updated AndroidWaiterUtils

--- src/main/java/appiumfactory/utilities/utils/AndroidWaiterUtils.java
+++ src/main/java/appiumfactory/utilities/utils/AndroidWaiterUtils.java
@@ -1,13 +1,20 @@
 package appiumfactory.utilities.utils;
 
+import appiumfactory.driver.AndroidDriverManager;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class AndroidWaiterUtils {
+
+    public AndroidWaiterUtils() {
+        PageFactory.initElements(new AppiumFieldDecorator(AndroidDriverManager.getAndroidDriver()), this);
+    }
 
     public void waitForVisibilityOfElement(AndroidDriver driver, int seconds, By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
