9dc87d0b142a84c4ca8f89bfe9711e70c0cf7641
anthonyhenderson <cuzixsin@gmail.com>
2025-07-12 13:54:15 -0700

    AndroidWaiterUtils dynamic wait utilities

--- src/main/java/appiumfactory/utilities/utils/AndroidWaiterUtils.java
+++ src/main/java/appiumfactory/utilities/utils/AndroidWaiterUtils.java
@@ -1,4 +1,27 @@
 package appiumfactory.utilities.utils;
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class AndroidWaiterUtils {
+
+    public void waitForVisibilityOfElement(AndroidDriver driver, int seconds, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void waitForInvisibilityOfElement(AndroidDriver driver, int seconds, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public void waitForElementToBeClickable(AndroidDriver driver, int seconds, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
 }
