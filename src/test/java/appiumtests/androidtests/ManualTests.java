678d4032ef6b24c96bec6259f1b476db3473c245
anthonyhenderson <cuzixsin@gmail.com>
2025-07-16 10:24:13 -0700

    new access'ibilityTest automates manual landing page test

---
+++ src/test/java/appiumtests/androidtests/ManualTests.java
@@ -0,0 +1,30 @@
+package appiumtests.androidtests;
+
+import appiumfactory.appiumcourse.apidemos.pages.AccessibilityPage;
+import basetests.BaseAPIDemosTests;
+import org.testng.annotations.Test;
+
+public class ManualTests extends BaseAPIDemosTests {
+
+    @Test
+    public void accessibilityPageTest() {
+        AccessibilityPage accessibilityPage = new AccessibilityPage();
+        accessibilityPage.isAPIDemosHeaderDisplayed()
+                .isAPIDemosIconsDisplayed()
+                .clickAccessibilityPageBtn()
+                .isAPIDemosHeaderDisplayed()
+                .isAPIDemosIconsDisplayed()
+                .clickNodeProviderBtn()
+                .isNodeProviderHeaderDisplayed()
+                .pressBackKey()
+                .clickNodeQueryingBtn()
+                .isNodeQueryHeaderDisplayed()
+                .pressBackKey()
+                .clickServiceBtn()
+                .isServiceHeaderDisplayed()
+                .pressBackKey()
+                .clickCustomViewBtn()
+                .isCustomViewHeaderDisplayed();
+    }
+
+}
