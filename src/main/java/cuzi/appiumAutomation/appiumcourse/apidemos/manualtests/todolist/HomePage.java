d8d5f5a36f7d03659879671dbad153fccb674bec
cuzi <cuzixsin@gmail.com>
2025-11-03 14:45:41 -0800

    new methods in HomePage for todolist app test

--- src/main/java/cuzi/appiumAutomation/appiumcourse/apidemos/manualtests/todolist/HomePage.java
+++ src/main/java/cuzi/appiumAutomation/appiumcourse/apidemos/manualtests/todolist/HomePage.java
@@ -1,6 +1,7 @@
 package cuzi.appiumAutomation.appiumcourse.apidemos.manualtests.todolist;
 
 import cuzi.appiumAutomation.base.BaseAndroidPage;
+import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 
 public class HomePage extends BaseAndroidPage {
@@ -13,6 +14,25 @@ public class HomePage extends BaseAndroidPage {
         androidIsDisplayed.isElementDisplayed(
                 By.xpath("//android.widget.TextView[@text='Todoist']"));
         return this;
+    }
+
+    public HomePage clickMoreOptionsBtn() {
+        androidClick.click(AppiumBy.accessibilityId("More options"));
+        return this;
+    }
+
+    public void clickSettingsBtn() {
+        androidClick.click(
+                By.xpath("//android.widget.TextView[@resource-id='com.todoist.remix:id/title' and @text='Settings']"));
+    }
+
+    public void clickAboutBtn() {
+        androidClick.click(
+                By.xpath("//android.widget.TextView[@resource-id='com.todoist.remix:id/title' and @text='About']"));
+    }
+
+    public void clickPlusBtn() {
+        androidClick.click(By.id("com.todoist.remix:id/addToDoItemFAB"));
     }
 
 }
---
+++ src/test/java/appiumtests/androidtests/TodoListTests.java
@@ -0,0 +1,8 @@
+package appiumtests.androidtests;
+
+import basetests.BaseTodoListTests;
+import cuzi.appiumAutomation.appiumcourse.apidemos.manualtests.todolist.HomePage;
+import org.testng.annotations.Test;
+
+public class TodoListTests extends BaseTodoListTests {
+}
