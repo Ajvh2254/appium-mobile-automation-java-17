990d250b2425c594f5793535be1e9747ce9e23e4
cuzi <cuzixsin@gmail.com>
2025-11-03 08:40:05 -0800

    moved tests from ManualTests class to APIDemosTests

--- src/test/java/appiumtests/androidtests/APIDemosTests.java
+++ src/test/java/appiumtests/androidtests/APIDemosTests.java
@@ -2,6 +2,8 @@ package appiumtests.androidtests;
 
 import cuzi.appiumAutomation.appiumcourse.apidemos.CommonPage;
 import basetests.BaseAPIDemosTests;
+import cuzi.appiumAutomation.appiumcourse.apidemos.manualtests.apidemos.AccessibilityPage;
+import cuzi.appiumAutomation.appiumcourse.apidemos.manualtests.apidemos.AnimationPage;
 import cuzi.appiumAutomation.appiumcourse.apidemos.pages.*;
 import org.testng.annotations.Test;
 
@@ -81,6 +83,73 @@ public class APIDemosTests extends BaseAPIDemosTests {
                 .clickOkBtn()
                 .pressAndroidBackKey()
                 .pressAndroidHomeKey();
+    }
+
+    @Test
+    public void accessibilityPageTest() {
+        AccessibilityPage accessibilityPage = new AccessibilityPage();
+        accessibilityPage.isAPIDemosHeaderDisplayed()
+                .isAPIDemosIconsDisplayed()
+                .clickAccessibilityPageBtn()
+                .isAPIDemosHeaderDisplayed()
+                .isAPIDemosIconsDisplayed()
+                .clickNodeProviderBtn()
+                .isNodeProviderHeaderDisplayed()
+                .pressAndroidBackKey()
+                .clickNodeQueryingBtn()
+                .isNodeQueryHeaderDisplayed()
+                .pressAndroidBackKey()
+                .clickServiceBtn()
+                .isServiceHeaderDisplayed()
+                .pressAndroidBackKey()
+                .clickCustomViewBtn()
+                .isCustomViewHeaderDisplayed();
+    }
+
+    @Test
+    public void animationPageTest() {
+        AnimationPage animationPage = new AnimationPage();
+        animationPage.isAPIDemosHeaderDisplayed()
+                .clickAnimationBtn()
+                .isAPIDemosTitleDisplayed()
+                .selectPageBtn("Bouncing Balls")
+                .isPageTitleDisplayed("Animation/Bouncing Balls")
+                .pressAndroidBackKey()
+                .isCloningBtnFocusable()
+                .isCloningBtnClickable()
+                .selectPageBtn("Cloning")
+                .isPageTitleDisplayed("Animation/Cloning")
+                .pressAndroidBackKey()
+                .selectPageBtn("Custom Evaluator")
+                .isPlayBtnSelectable()
+                .pressAndroidBackKey()
+                .selectPageBtn("Default Layout Animations")
+                .isAddBtnClickable()
+                .pressAndroidBackKey()
+                .selectPageBtn("Events")
+                .isPageTitleDisplayed("Animation/Events")
+                .pressAndroidBackKey()
+                .selectPageBtn("Hide-Show Animations")
+                .isPageTitleDisplayed("Animation/Hide-Show Animations")
+                .pressAndroidBackKey()
+                .selectPageBtn("Layout Animations")
+                .isPageTitleDisplayed("Animation/Layout Animations")
+                .pressAndroidBackKey()
+                .selectPageBtn("Loading")
+                .isPageTitleDisplayed("Animation/Loading")
+                .pressAndroidBackKey()
+                .selectPageBtn("Multiple Properties")
+                .isPageTitleDisplayed("Animation/Multiple Properties")
+                .pressAndroidBackKey()
+                .selectPageBtn("Reversing")
+                .isPageTitleDisplayed("Animation/Reversing")
+                .pressAndroidBackKey()
+                .selectPageBtn("Seeking")
+                .isPageTitleDisplayed("Animation/Seeking")
+                .pressAndroidBackKey()
+                .selectPageBtn("View Flip")
+                .isPageTitleDisplayed("Animation/View Flip")
+                .pressAndroidBackKey();
     }
 
 }
