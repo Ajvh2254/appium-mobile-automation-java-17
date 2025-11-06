78819e64e2b2541c79e55cfe84aa6a915b3ec16f
cuzi <cuzixsin@gmail.com>
2025-11-06 10:03:15 -0800

    new todolist app test sets reminder

--- src/main/java/cuzi/appiumAutomation/appiumcourse/apidemos/manualtests/todolist/InputPage.java
+++ src/main/java/cuzi/appiumAutomation/appiumcourse/apidemos/manualtests/todolist/InputPage.java
@@ -1,6 +1,34 @@
 package cuzi.appiumAutomation.appiumcourse.apidemos.manualtests.todolist;
 
 import cuzi.appiumAutomation.base.BaseAndroidPage;
+import cuzi.appiumAutomation.utilities.androidutils.AndroidSendKeys;
+import org.openqa.selenium.By;
 
 public class InputPage extends BaseAndroidPage {
+
+    public InputPage() {
+        super();
+    }
+
+    public InputPage isTitleHeaderDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.id("com.todoist.remix:id/userToDoEditText"));
+        return this;
+    }
+
+    public InputPage sendTitleKeys() {
+        AndroidSendKeys.sendKeys(By.id("com.todoist.remix:id/userToDoEditText"), "New Reminder");
+        return this;
+    }
+
+    public InputPage sendDescriptionKeys() {
+        AndroidSendKeys.sendKeys(
+                By.id("com.todoist.remix:id/userToDoDescription"), "Important meeting");
+        return this;
+    }
+
+    public InputPage clickSliderToggle() {
+        androidClick.click(By.className("android.widget.Switch"));
+        return this;
+    }
+
 }
