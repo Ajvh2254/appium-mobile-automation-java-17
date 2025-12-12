9f5a371b72ea12431b4a43ca7f82e26bb0d75472
cuzi <cuzixsin@gmail.com>
2025-12-11 17:53:33 -0800

    new tutorialPageTest tests the functionality of the tutorial pages

--- src/main/java/cuzi/appiumAutomation/appiumcourse/manualtests/todo/TutorialPage.java
+++ src/main/java/cuzi/appiumAutomation/appiumcourse/manualtests/todo/TutorialPage.java
@@ -13,9 +13,10 @@ import java.util.List;
 @Log4j2
 public class TutorialPage extends BaseAndroidPage {
 
+    By nextBtn = By.id("org.secuso.privacyfriendlytodolist:id/btn_next");
+
     public String isWelcomeTextDisplayed() {
         By welcomeHeader = By.xpath("//*[@text='Welcome!']");
-        log.info(welcomeHeader + " is displayed");
         // Using "current node" represented with an asterisk Example: //*[]
         return AndroidIsTextDisplayed.getText(welcomeHeader);
     }
@@ -40,7 +41,6 @@ public class TutorialPage extends BaseAndroidPage {
     }
 
     public boolean isNextBtnClickable() {
-        By nextBtn = By.id("org.secuso.privacyfriendlytodolist:id/btn_next");
         try {
             AndroidWaiter.waitForElementToBeClickable(nextBtn);
             log.info("{} is clickable", nextBtn);
@@ -49,6 +49,58 @@ public class TutorialPage extends BaseAndroidPage {
             log.info(nextBtn + " is not clickable");
             return false;
         }
+    }
+
+    public void clickNextBtn() {
+        androidClick.click(nextBtn);
+    }
+
+    public String isListsHeaderDisplayed() {
+        By listsHeader = By.xpath("//*[@text='Lists']");
+        return AndroidIsTextDisplayed.getText(listsHeader);
+    }
+
+    public boolean isListsTextDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.xpath(
+                "//android.widget.TextView[@text='You can manage your tasks in labeled To-Do lists.']"));
+        return true;
+    }
+
+    public boolean isReminderHeaderDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.xpath("//android.widget.TextView[@text='Reminder']"));
+        return true;
+    }
+
+    public String isReminderTextDisplayed() {
+        By reminderText = By.xpath(
+                "//android.widget.TextView[@text='You can define deadlines and reminders for your tasks.']");
+        return AndroidIsTextDisplayed.getText(reminderText);
+    }
+
+    public String isEverythingHeaderDisplayed() {
+        By everythingHeader = By.xpath("//android.widget.TextView[@text='Everything is important']");
+        return AndroidIsTextDisplayed.getText(everythingHeader);
+    }
+
+    public boolean isEverythingTextDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.xpath(
+                "//android.widget.TextView[@text='You can add subtasks and the progress of their main task. Progress can be set manually or automatically by the number of done subtasks.']"));
+        return true;
+    }
+
+    public String isSecureHeaderDisplayed() {
+        By secureHeader = By.xpath("//android.widget.TextView[@text='Secure']");
+        return AndroidIsTextDisplayed.getText(secureHeader);
+    }
+
+    public boolean isSecureTextDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.xpath(
+                "//android.widget.TextView[@text='You can add a PIN-protection in the settings.']"));
+        return true;
+    }
+
+    public void clickOkayBtn() {
+        androidClick.click(nextBtn);
     }
 
 }
--- src/test/java/appiumtests/androidtests/TodoTests.java
+++ src/test/java/appiumtests/androidtests/TodoTests.java
@@ -8,13 +8,31 @@ import org.testng.annotations.Test;
 public class TodoTests extends BaseTodoTests {
 
     @Test
-    public void tutorialPageTest() {
+    public void landingPageTest() {
         TutorialPage tutorialPage = new TutorialPage();
         Assert.assertEquals(tutorialPage.isWelcomeTextDisplayed(), "Welcome!");
         Assert.assertTrue(tutorialPage.isAdvertisingTextDisplayed());
         Assert.assertTrue(tutorialPage.isSkipBtnDisplayed());
         tutorialPage.isPageIndicatorDisplayed();
         Assert.assertTrue(tutorialPage.isNextBtnClickable());
+    }
+
+    @Test
+    public void tutorialPageTest() {
+        TutorialPage tutorialPage = new TutorialPage();
+        tutorialPage.clickNextBtn();
+        Assert.assertEquals(tutorialPage.isListsHeaderDisplayed(), "Lists");
+        Assert.assertTrue(tutorialPage.isListsTextDisplayed());
+        tutorialPage.clickNextBtn();
+        Assert.assertTrue(tutorialPage.isReminderHeaderDisplayed());
+        Assert.assertEquals(tutorialPage.isReminderTextDisplayed(), "You can define deadlines and reminders for your tasks.");
+        tutorialPage.clickNextBtn();
+        Assert.assertEquals(tutorialPage.isEverythingHeaderDisplayed(), "Everything is important");
+        Assert.assertTrue(tutorialPage.isEverythingTextDisplayed());
+        tutorialPage.clickNextBtn();
+        Assert.assertEquals(tutorialPage.isSecureHeaderDisplayed(),"Secure");
+        Assert.assertTrue(tutorialPage.isSecureTextDisplayed());
+        tutorialPage.clickOkayBtn();
     }
 
 }
