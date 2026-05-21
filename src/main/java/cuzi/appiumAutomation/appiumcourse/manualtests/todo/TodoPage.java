efb8029331ce645a7fc8d30ffadfd43117b8fde6
cuzi <cuzixsin@gmail.com>
2026-05-20 20:31:22 -0700

    TodoPage tests functions of todo app using isDisplayed and asserts

---
+++ src/main/java/cuzi/appiumAutomation/appiumcourse/manualtests/todo/TodoPage.java
@@ -0,0 +1,36 @@
+package cuzi.appiumAutomation.appiumcourse.manualtests.todo;
+
+import cuzi.appiumAutomation.base.BaseAndroidPage;
+import cuzi.appiumAutomation.driver.AndroidDriverManager;
+import cuzi.appiumAutomation.utilities.androidutils.AndroidIsTextDisplayed;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class TodoPage extends BaseAndroidPage {
+
+    public boolean isWelcomeTextDisplayed() {
+        androidIsDisplayed.isElementDisplayed(By.xpath("//android.widget.TextView[@text='Welcome!']"));
+        return true;
+    }
+
+    public String isSkipBtnDisplayed() {
+        By skipBtn = By.id("org.secuso.privacyfriendlytodolist:id/btn_skip");
+        return AndroidIsTextDisplayed.getText(skipBtn);
+    }
+
+    public void isPageIndicatorDisplayed() {
+        By pageIndicator = By.xpath("(//android.widget.TextView[@text='•'])");
+        List<WebElement> pageIndicators = AndroidDriverManager.getAndroidDriver().findElements(pageIndicator);
+        for (int i = 0; i < pageIndicators.size(); i++) {
+            androidIsDisplayed.isElementDisplayed(pageIndicator);
+        }
+    }
+
+    public String isListsHeaderDisplayed() {
+        By listsHeader = By.xpath("//android.widget.TextView[@text='Lists']");
+        return AndroidIsTextDisplayed.getText(listsHeader);
+    }
+
+}
